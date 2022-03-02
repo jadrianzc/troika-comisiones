b8a10b3be435b69524ceeda87fce1c93fb453567
jadrianzc <jadrianzc99@gmail.com>
2022-03-02 12:12:04 -0500

    cambios maquetado

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,4 +5,12 @@ router.get('/', (req, res) => {
 	res.render('index');
 });
 
+router.get('/factura', (req, res) => {
+	res.render('factura');
+});
+
+router.get('/informe', (req, res) => {
+	res.render('index');
+});
+
 module.exports = router;
